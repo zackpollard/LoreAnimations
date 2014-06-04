e8a62bf8053c82ada18efcfdfc050c0beffca731
zackpollard <zackpollard@ymail.com>
2014-06-04 05:46:26 +0100

    Updated LoreAnimation.java for multiple items.
    I have edited LoreAnimations.java so that it can work for any item ID. This is simple to implement. The format remains the same as in the apple.txt, but the name is now changed to ID.txt. I have also moved the inventory list scan from the onEnable method into the onOpenInventory and onCloseInventory methods in order for it to work for new players that join after reload (which shouldn't be used). This should be working, but it is untested. Make a copy of your old cope and then test this code. If there's any issues shout at me in that thread.

--- LoreAnimations/src/com/github/whitehooder/LoreAnimations/LoreAnimations.java
+++ LoreAnimations/src/com/github/whitehooder/LoreAnimations/LoreAnimations.java
@@ -4,12 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.bukkit.Material;
@@ -18,6 +18,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.Inventory;
@@ -28,96 +30,101 @@ import org.bukkit.scheduler.BukkitScheduler;
 
 public class LoreAnimations extends JavaPlugin implements Listener {
 
-	List<Inventory> invlist = new ArrayList<Inventory>();
-	int framecounter = 0;
+	public List<Inventory> invlist = new ArrayList<Inventory>();
+	//ItemID, FrameCount
+	public HashMap<Integer, Integer> framecounter = new HashMap<Integer, Integer>();
+	//int framecounter = 0;
 
-	ArrayList<String> frame = new ArrayList<String>();
-
-	ArrayList<ArrayList<String>> animation = new ArrayList<ArrayList<String>>();
+	public HashMap<Integer, ArrayList<ArrayList<String>>> animation = new HashMap<Integer, ArrayList<ArrayList<String>>>();
 
 	@Override
 	public void onDisable() {
-
+		
 	}
 
 	@Override
 	public void onEnable() {
 
-		for (World w : getServer().getWorlds()) {
-			for (Player p : w.getPlayers()) {
-				invlist.add(p.getInventory());
-			}
-		}
-
-		File apple = new File(getDataFolder().getPath() + File.separator
-				+ "apple.txt");
-		try {
-			// final StringBuilder stringBuilder = new StringBuilder();
-			InputStream inStream = new FileInputStream(getDataFolder()
-					+ File.separator + "apple.txt");
-			InputStreamReader streamReader;
-			try {
-				streamReader = new InputStreamReader(inStream, "UTF-8");
-				final BufferedReader bufferedReader = new BufferedReader(
-						streamReader);
-				String line = "";
+		/*
+		 *  for (World w : getServer().getWorlds()) {
+		 *	 for (Player p : w.getPlayers()) {
+		 *	 	invlist.add(p.getInventory());
+		 *	 }
+		 *}
+		 */
+		for(File file : this.getDataFolder().listFiles()) {
+			
+			if(file.getAbsolutePath().endsWith(".txt")) {
+				
+				String[] split = file.getAbsolutePath().split(".");
 				try {
-					while ((line = bufferedReader.readLine()) != null) {
-						if (line.equals("==FRAME==")) {
-							animation.add(new ArrayList<String>(frame));
-							frame.clear();
-						} else {
-							frame.add(colorize(line));
+					int itemid  = Integer.parseInt(split[0]);
+					
+					try {
+						InputStream inputStream = new FileInputStream(file);
+						InputStreamReader streamReader;
+						try {
+							
+							streamReader = new InputStreamReader(inputStream, "UTF-8");
+							final BufferedReader bufferedReader = new BufferedReader(streamReader);
+							String line = "";
+							
+							try {
+								ArrayList<String> frame = new ArrayList<String>();
+								ArrayList<ArrayList<String>> frames = new ArrayList<ArrayList<String>>();
+								while((line = bufferedReader.readLine()) != null) {
+									if (line.equals("==FRAME==")) {
+										frames.add((new ArrayList<String>(frame)));
+										frame.clear();
+									} else {
+										
+										frame.add(colorize(line));
+									}
+								}
+								animation.put(itemid, frames);
+							} catch (IOException e) {
+								
+								e.printStackTrace();
+							}
+						} catch (UnsupportedEncodingException e) {
+							
+							e.printStackTrace();
 						}
+					} catch (FileNotFoundException e) {
+						
+						e.printStackTrace();
 					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					
+				} catch(NumberFormatException e) {
+						
+						System.err.println("[LoreAnimations] - The file " + file.getName() + " did not begin with an integer (itemid).");
 				}
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-			// BufferedReader reader = new BufferedReader(new FileReader(
-			// getDataFolder() + File.separator + "apple.txt"));
-			// String str = null;
-			// try {
-			// while ((str = reader.readLine()) != null) {
-			// if (str.equals("==FRAME==")) {
-			// animation.add(new ArrayList<String>(frame));
-			// frame.clear();
-			// } else {
-			//
-			// frame.add(colorize(line));
-			// }
-			// }
-			// } catch (IOException e) {
-			// e.printStackTrace();
-			// }
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		}
-
+		
 		BukkitScheduler scheduler = getServer().getScheduler();
 		scheduler.runTaskTimer(this, new Runnable() {
+			@SuppressWarnings("deprecation")
 			@Override
 			public void run() {
 				for (Inventory inv : invlist) {
 					for (ItemStack item : inv.getContents()) {
 						if (item != null) {
-							if (item.getType() == Material.APPLE) {
+							if (animation.containsKey(item.getTypeId())) {
+								framecounter.put(item.getTypeId(), 0);
 								ItemMeta meta = item.getItemMeta();
-								meta.setLore(animation.get(framecounter++));
+								meta.setLore(animation.get(item.getTypeId()).get(framecounter.get(item.getTypeId()) + 1));
+								framecounter.put(item.getTypeId(), framecounter.get(item.getTypeId()) + 1);
 								item.setItemMeta(meta);
-								if (framecounter > animation.size() - 1) {
-									framecounter = 0;
+								if (framecounter.get(item.getTypeId()) > animation.size() - 1) {
+									framecounter.put(item.getTypeId(), 0);
 								}
 							}
 						}
 					}
 				}
 			}
-		}, 2, 2);
+		}, 1, 1);
 		getServer().getPluginManager().registerEvents(this, this);
 	}
 
@@ -126,7 +133,19 @@ public class LoreAnimations extends JavaPlugin implements Listener {
 		msg = msg + "§r";
 		return msg;
 	}
-
+	
+	@EventHandler
+	public void onOpenInventory(InventoryOpenEvent event) {
+		
+		invlist.add(event.getInventory());
+	}
+	
+	@EventHandler
+	public void onCloseInventory(InventoryCloseEvent event) {
+		
+		invlist.remove(event.getInventory());
+	}
+	
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onJoin(PlayerJoinEvent e) {
 		invlist.add(e.getPlayer().getInventory());
